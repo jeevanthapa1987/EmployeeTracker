32e68b97bd075e99d5594864a5febe1c16617799
jeevanthapa1987 <83832318+jeevanthapa1987@users.noreply.github.com>
2021-10-17 10:07:43 +0545

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,9 +10,6 @@ VALUES
 ('Full Stack Developer', 80000, 1),
 ('Software Engineer', 120000, 1),
 ('Accountant', 10000, 2), 
-('Finanical Analyst', 150000, 2),
-('Marketing Coordindator', 70000, 3), 
-('Sales Lead', 90000, 3),
 ('Project Manager', 100000, 4),
 ('Operations Manager', 90000, 4);
 
@@ -23,9 +20,6 @@ VALUES
 ('Devin', 'Anderson', 1, 1),
 ('Mary', 'Brown', 4, null),
 ('Ashley', 'Jones', 3, 3),
-('Tyler', 'Moore', 6, null),
-('Ana', 'Sanchez', 5, 5),
-('Lewis', 'Allen', 7, null),
-('Katherine', 'Green', 8, 7);
+('Tyler', 'Moore', 6, null)
 
 
